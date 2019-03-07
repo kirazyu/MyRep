d21e143340d1b0d90b1bf266ea2191cf75b9d00d
kirazyu <48295224+kirazyu@users.noreply.github.com>
2019-03-07 09:17:01 +0300

    Update LongestWordArray.java
    changes changes

--- LongestWordArray.java
+++ LongestWordArray.java
@@ -4,36 +4,27 @@ import java.util.Scanner;
 
 public class LongestWordArray {
 
-    public LongestWordArray() {
+    public static void main (String[]args){
 
-    }
 
-    public static String longestWord() {
-        //User input for defining the number or elements in array
+        System.out.println("Welcome to the Longest Word program!");
 
         Scanner input = new Scanner(System.in);
         System.out.println("How long do you want your array to be? Enter a number of words: ");
-
-
         int amountOfWords = Integer.parseInt(input.nextLine());
+        System.out.println("Your array size is " + amountOfWords);
 
         //creating an empty array
         String[] myArr = new String[amountOfWords];
 
-        System.out.println("Your array size is " + amountOfWords);
-
-        //loop for inputting every word one by one and storing it in the array
-        for (int j = 0; j < myArr.length; j++) {
-            System.out.println("Enter the " + j + " word: ");
-            myArr[j] = input.nextLine();
-
+        for (int i = 0; i < myArr.length; i++){
+            System.out.println("Enter the " + (i+1) + " word: ");
+            myArr[i] = input.nextLine();
         }
-        return myArr.toString();
-}
-    public static void main (String[]args){
 
-
-        System.out.println("The longest word in the array is " + longestWord());
+        System.out.println("The words in array are: ");
+        for (String word : myArr){
+            System.out.println(word);
+        }
     }
 }
-
