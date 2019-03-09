bb9bae4400e6ef60324763f17926880d0eb25000
kirazyu <48295224+kirazyu@users.noreply.github.com>
2019-03-09 12:25:26 +0300

    Update HW2 --  Box.java

--- HW2 --  Box.java
+++ HW2 --  Box.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 
 public interface Box {
 
-    Sweets get(int index);
+// чому не работает
+//  Sweets get(int index);
     void gatherCandy(Sweets sweets);
     void information ();
     double totalPrice ();
